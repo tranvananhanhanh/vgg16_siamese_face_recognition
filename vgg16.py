8c71bbf68c25f7dc6665d2d8c8ff4e61f6b88522
tranvananhanhanh <139228186+tranvananhanhanh@users.noreply.github.com>
2023-12-21 20:07:19 +0700

    Update vgg16.py

--- vgg16.py
+++ vgg16.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 
 
 
-# Tạo mô hình VGG16
 vgg16_model = vgg16.VGG16(weights='imagenet', include_top=False,input_shape = (128, 128, 3))
 # Đóng băng 4 block đầu
 for layer in vgg16_model.layers[:15]:
