0df4405ee49d94467c86269c592dc512ebd01da7
tranvananhanhanh <139228186+tranvananhanhanh@users.noreply.github.com>
2023-12-21 20:08:31 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,15 +1,15 @@
 from data_for_train import data,preprocess_twin
 
 
-# Build dataloader pipeline
+
 data = data.map(preprocess_twin)
 data = data.cache()
 data = data.shuffle(buffer_size=10000)
-# Training partition
+
 train_data = data.take(round(len(data)*.7))
 train_data = train_data.batch(16)
 train_data = train_data.prefetch(8)
-# Testing partition
+
 test_data = data.skip(round(len(data)*.7))
 test_data = test_data.take(round(len(data)*.3))
 test_data = test_data.batch(16)
